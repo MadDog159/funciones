f2b8f6a8bb22f27de1ea576a0f193b90c141a48d
MadDog159 <77283910+MadDog159@users.noreply.github.com>
2021-02-20 22:37:23 -0400

    first commit

---
+++ funciones.py
@@ -0,0 +1,20 @@
+
+def mono():
+    return zapatico
+
+def correr():
+    camisita = "adidas"
+    def cambiarcamisa():
+        camisita = "nike"
+        print("micamisa es: "+ camisita)
+        return camisita
+
+    def camisa():
+        print("mi camisa es: "+ camisita)
+    camisita = cambiarcamisa()
+    camisita = "pene"
+    camisa()
+    
+# print(cuarto)    
+zapatico = "nike"
+correr()    
